5cf0443a13f3aeabd0ceb62424039b5c412de37f
RicoSchmitt <rico.schmitt@charite.de>
2024-08-26 11:44:55 +0200

    Removed test files which will be applied at a later stage in the project

--- pseudo_test.py
+++
@@ -1,6 +0,0 @@
-# Test pipeline 
-# Test for variable names
-# Variance check with original values - check with domain experts for reasonable values - with feedback
-# test for clinically implausible values
-# comparison y-data profile - datasets comparison for different cohorts (mimic, eicu etc.)
-
